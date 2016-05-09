ceb7c7665c83f4b655f621c980d27ae94d905429
Jonatandb <jonatandb@gmail.com>
2016-05-09 19:17:56 -0300

    Commit de prueba
    Agregado a .gitignore de archivo o y ejecutable principal.
    Modificaciones menores en main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
 using namespace std;
+
 //CLASES
 #include "inc/cls/Random.h"
 #include "inc/cls/Vector.h"
+
 //FUNCIONES
 #include "inc/func/vec.h"
 
 /** TODO (angel#1#): Repensar SDL */
 void mostrarResultados(int r1, int r2){
-  short i;
-  cout << "\t";
-  for (i=0; i<r1; i++){
-    cout << "O ";
-  }
-  for (i=0; i<r2; i++){
-    cout << "# ";
-  }
+   short i;
+   cout << "\t";
+   for (i=0; i<r1; i++){
+      cout << "O ";
+   }
+   for (i=0; i<r2; i++){
+      cout << "# ";
+   }
 }
-int main(){
-    Random::inicializar();
-    Vector v(4);
-    poblarVector(&v, 5, true);
-    mostrarVector(v);
-    cout << endl << endl;
-    while(true){
+
+int main()
+{
+   Random::inicializar();
+   Vector v(4);
+   cout<<"Poblando vector..."<<endl;
+   poblarVector(&v, 5, true);
+   cout<<"Contenido del vector:"<<endl;
+   mostrarVector(v);
+   cout << endl << endl;
+   while(true){
       Vector vGuess(4);
       short guess, i, res1=0, res2=0;
       for(i=0; i<4; i++){
-        cin >> guess;
-        vGuess.set(i, guess);
+         cin >> guess;
+         vGuess.set(i, guess);
       }
       chequearGuess(v, vGuess, &res1, &res2);
       mostrarResultados(res1, res2);
       cout << endl;
-    }
-    return 0;
+   }
+   return 0;
 }
